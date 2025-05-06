6370bc7fddfe54e96857323065b937c4a4c52e51
02Alexis <m_chael1012@hotmail.com>
2025-05-06 10:12:11 -0500

    creación de componente para las categorias

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import MainBanner from '../components/MainBanner'
+import MainBanner from '../components/MainBanner.jsx'
+import Categories from '../components/Categories.jsx'
 
 const Home = () => {
   return (
     <div className='mt-10'>
         <MainBanner/>
+        <Categories/>
     </div>
   )
 }
